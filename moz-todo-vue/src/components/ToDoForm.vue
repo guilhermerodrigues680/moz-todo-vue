4b22154665607ef4c183574bae9f86403177507e
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-11-06 14:59:19 -0300

    Tutorial Vue.js Mozilla
    * Parei aqui: https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Vue_computed_properties

---
+++ moz-todo-vue/src/components/ToDoForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="onSubmit" class="custom-checkbox">
+    <label for="new-todo-input" class="label__lg">
+      What needs to be done?
+    </label>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model.lazy.trim="label"
+      class="input__lg"
+    />
+    <button type="submit" class="btn btn__primary btn__lg">
+      Add
+    </button>
+  </form>
+</template>
+    
+<script>
+  export default {
+    methods: {
+      onSubmit() {
+        console.log('Label value: ', this.label);
+
+        if(this.label === "") {
+          return;
+        }
+
+        this.$emit("todo-added", this.label);
+        this.label = "";
+      }
+    },
+    data() {
+      return {
+        label: ""
+      };
+    }
+  };
+</script>
